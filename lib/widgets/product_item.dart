a63882e0b86fcc37a63b6dec280e29a52bae39d7
Stefan <stefannemi16@gmail.com>
2021-12-01 18:48:17 +0200

    Video 189. Done

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({Key? key, required this.product}) : super(key: key);
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          leading: IconButton(
+            icon: const Icon(Icons.favorite),
+            onPressed: () {  },
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {  },
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          backgroundColor: Colors.black87,
+        ),
+
+      ),
+    );
+  }
+}
